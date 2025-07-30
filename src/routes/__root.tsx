f6de86c03f26bda9a3244e03d7135e9c752f982f
Piet de Vries <pietdevries94@gmail.com>
2025-07-30 10:29:31 +0200

    style: update body and main element classes for consistent theming

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<main className="min-h-screen p-6 bg-background text-foreground inset-shadow-[0_0_16px_8px] inset-shadow-amber-800/10 font-serif">
+			<main>
 				<Outlet />
 			</main>
 			<TanStackRouterDevtools />
