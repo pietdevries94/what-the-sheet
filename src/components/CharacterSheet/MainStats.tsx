e252a8c2a8b6394fdd76057c326d872e5032ccf9
Piet de Vries <pietdevries94@gmail.com>
2025-07-31 08:01:03 +0200

    Center text in main stats

--- src/components/CharacterSheet/MainStats.tsx
+++ src/components/CharacterSheet/MainStats.tsx
@@ -29,16 +29,16 @@ const StatBlock: React.FC<{ label: string; value: number | null }> = ({
 	const modifier = value ? Math.floor((value - 10) / 2) : undefined;
 	return (
 		<div className="flex flex-col items-center justify-center relative py-2">
-			<span className="text-sm tracking-wide text-stone-500 absolute top-0 border-stone-400 bg-background leading-none py-0.5 h-5 text-center w-9 rounded-md border">
+			<div className="flex items-center justify-center text-sm tracking-wide text-stone-500 absolute top-0 border-stone-400 leading-none bg-background h-5 text-center w-9 rounded-md border">
 				{label}
-			</span>
+			</div>
 			<span className="flex items-center justify-center w-12 h-14 rounded-md border border-stone-400 text-lg text-stone-600">
 				{modifier && modifier >= 0 ? "+" : ""}
 				{modifier ?? ""}
 			</span>
-			<span className="text-sm text-stone-500 absolute bottom-0 bg-background border-stone-400 leading-none py-0.5 h-5 w-6 text-center rounded-md border">
+			<div className="flex items-center justify-center text-sm text-stone-500 absolute bottom-0 bg-background border-stone-400 leading-none h-5 w-6 text-center rounded-md border">
 				{value}
-			</span>
+			</div>
 		</div>
 	);
 };
