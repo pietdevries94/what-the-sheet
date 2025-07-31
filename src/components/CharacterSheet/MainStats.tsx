6181a5320b6716dd926ceb5a8eea21fc2eee7534
Piet de Vries <pietdevries94@gmail.com>
2025-07-31 07:51:55 +0200

    Fix leading inconsistency in mainstat on iOS

--- src/components/CharacterSheet/MainStats.tsx
+++ src/components/CharacterSheet/MainStats.tsx
@@ -29,14 +29,14 @@ const StatBlock: React.FC<{ label: string; value: number | null }> = ({
 	const modifier = value ? Math.floor((value - 10) / 2) : undefined;
 	return (
 		<div className="flex flex-col items-center justify-center relative py-2">
-			<span className="text-sm tracking-wide text-stone-500 absolute top-0 border-stone-400 bg-background leading-3.5 py-0.5 h-5 text-center w-9 rounded-md border">
+			<span className="text-sm tracking-wide text-stone-500 absolute top-0 border-stone-400 bg-background leading-none py-0.5 h-5 text-center w-9 rounded-md border">
 				{label}
 			</span>
 			<span className="flex items-center justify-center w-12 h-14 rounded-md border border-stone-400 text-lg text-stone-600">
 				{modifier && modifier >= 0 ? "+" : ""}
 				{modifier ?? ""}
 			</span>
-			<span className="text-sm text-stone-500 absolute bottom-0 bg-background border-stone-400 leading-3.5 py-0.5 h-5 w-6 text-center rounded-md border">
+			<span className="text-sm text-stone-500 absolute bottom-0 bg-background border-stone-400 leading-none py-0.5 h-5 w-6 text-center rounded-md border">
 				{value}
 			</span>
 		</div>
